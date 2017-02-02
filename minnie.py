4459d742a8d22749b67d2248b4e36dd4e673782f
booyaa <booyaabooyaabooyaa@gmail.com>
2017-02-02 09:00:28 +0000

    separating out missing minute archival

---
+++ minnie.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""
+minute maker - finds missing rust community team minutes
+via meeting agendas raised in "team" repo's issues
+"""
+
+import logging
+import datetime as dt
+import os
+from simpledate import SimpleDate
+from github import Github
+
+def main():
+    print("helo world!")
+
+if __name__ == "__main__":
+    main()
