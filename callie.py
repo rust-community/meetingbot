906375c7097edea9135aa3267a8c1b8b51aa7cae
booyaa <booyaabooyaabooyaa@gmail.com>
2017-05-19 08:32:15 +0100

    refactor

--- callie.py
+++ callie.py
@@ -10,7 +10,6 @@ from datetime import datetime, timezone
 from dateutil import tz
 
 from icalendar import Calendar
-# from ics import Calendar
 
 import pytz
 import requests
@@ -22,65 +21,8 @@ references:
 - https://en.wikipedia.org/wiki/ICalendar
 """
 
-# def old_main():
-#     url = "https://calendar.google.com/calendar/ical/apd9vmbc22egenmtu5l6c5jbfc%40group.calendar.google.com/private-5c6397513ba5958def67f9dde521e6f4/basic.ics" # pylint: disable=line-too-long
-#     cal = Calendar(requests.get(url).text)
-#     event_list = cal.events.today()
-#     print("Found {} events for today".format(len(event_list)))
-#     event_name = "Rust Community Team Meeting"
-#     event_name = "Rust Utrecht"
 
-#     for e in event_list:
-#         if e.name == event_name:
-#             local_time = e.begin.to("Europe/London")
-#             if e.begin.humanize() == "in an hour":
-#                 print("send out reminder!")
-#                 print("Event '{}' start {}".format(e.name, e.begin.humanize()))
-#                 print("\tlocal begin time: ", local_time)
-#                 print("\traw begin {}\n\traw end {}".format(e.begin, e.end))
-#             else:
-
-#                 print("\tnot yet time, sleeping...({})".format(local_time.humanize()))
-
-
-def main_ics():
-    # url =
-    # "https://calendar.google.com/calendar/ical/apd9vmbc22egenmtu5l6c5jbfc%40group.calendar.google.com/private-5c6397513ba5958def67f9dde521e6f4/basic.ics"
-    # # pylint: disable=line-too-long
-    url = "http://0.0.0.0:8080/basic.ics"
-    cal = Calendar(requests.get(url).text)
-
-    events = cal.events.on("2017-04-19")  # normally .today()
-    # events = cal.events.today()
-    if len(events) == 0:
-        print("no data.")
-        return
-
-    for event in events:
-        if event.name == "Rust Community Team Meeting":
-            print(event.begin, event.begin.humanize())
-            # print(event)
-
-# def main_icalendar():
-#     # source: http://stackoverflow.com/a/3408488/105282
-#     # if you don't want to use icalendar, vobject is another useful one
-#     # http://stackoverflow.com/a/6470135/105282
-#     calendar_file = os.path.join('dontcommitmebro', 'basic.ics')
-#     with open(calendar_file, 'rb') as fp:
-#         data = fp.read()
-
-#     cal = icalendar.Calendar.from_ical(data)
-
-#     for comp in cal.walk('vevent'):
-#         if comp.decoded('summary') == b'Rust Community Team Meeting':
-#             repeat = comp.get('rrule')
-#             if repeat is not None:
-#                 rule = comp.decoded('rrule')
-#             else:
-#                 rule = "not supported"
-
-
-def main_spike():
+def main():
     """
     - icalendar usage - https://github.com/pybites/bday-app/blob/master/bdays.py
     - convert local time to utc - http://stackoverflow.com/a/25662061/105282
@@ -107,10 +49,8 @@ def main_spike():
             print("dtstart: {}\ndescription: {}".format(
                 start_time, comp.get('DESCRIPTION')))
 
-            
             print(rrule)
             print("--------------")
+
 if __name__ == "__main__":
-    # main_icalendar()
-    # main_ics()
-    main_spike()
+    main()
