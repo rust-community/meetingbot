d316000fd9f2946efcc4cd033bba6ef6fb56036a
Mark Sta Ana <booyaabooyaabooyaa@gmail.com>
2017-07-13 14:55:17 +0100

    feat: repurposed script to send messages

---
+++ say_it.sh
@@ -0,0 +1,16 @@
+#!/bin/bash +xv
+if [ -z "$HASHPIPE_BIN" ];
+then
+	echo HASHPIPE_BIN not defined!
+	exit 1
+fi
+
+killall $HASHPIPE_BIN # seem to hang around last time
+
+BOT_NAME="meetingbot"
+BOT_SERVER="irc.mozilla.org"
+#BOT_CHANNEL='#sekrit31337'
+BOT_CHANNEL='#rust-community'
+BOT_MESSAGE=$@
+
+echo ${BOT_MESSAGE} | $HASHPIPE_BIN --nick ${BOT_NAME} --server ${BOT_SERVER} --channels ${BOT_CHANNEL}
